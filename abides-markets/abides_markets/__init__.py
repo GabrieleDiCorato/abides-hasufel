1607578c7d07dbd82a56014519786fd3a767f8cb
Gabriele Di Corato <gabriele.dicorato@gmail.com>
2026-03-20 18:15:00 +0100

    Documented mypy suppressions, updated pre-commit hooks to latest, populated abides_markets public API

---
+++ abides-markets/abides_markets/__init__.py
@@ -0,0 +1,35 @@
+"""ABIDES-Markets: financial market simulation built on ABIDES-Core."""
+
+from .agents import (
+    AdaptiveMarketMakerAgent,
+    ExchangeAgent,
+    FinancialAgent,
+    MomentumAgent,
+    NoiseAgent,
+    POVExecutionAgent,
+    TradingAgent,
+    ValueAgent,
+)
+from .order_book import OrderBook
+from .orders import LimitOrder, MarketOrder, Order, Side
+from .price_level import PriceLevel
+
+__all__ = [
+    # Agents
+    "AdaptiveMarketMakerAgent",
+    "ExchangeAgent",
+    "FinancialAgent",
+    "MomentumAgent",
+    "NoiseAgent",
+    "POVExecutionAgent",
+    "TradingAgent",
+    "ValueAgent",
+    # Order book
+    "OrderBook",
+    "PriceLevel",
+    # Orders
+    "LimitOrder",
+    "MarketOrder",
+    "Order",
+    "Side",
+]
